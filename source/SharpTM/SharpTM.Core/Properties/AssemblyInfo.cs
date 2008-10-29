990c37f9edc531e0d9db2fb4fd683db67547349b
MarcelHoyer <MarcelHoyer@localhost>
2008-10-29 23:58:02 +0000

    * modified default namespace from Pixelplastic.TopicMaps.SharpTM.Core to Pixelplastic.TopicMaps.SharpTM * renamed project from SharpTM.Core to SharpTM * renamed assembly output name from Pixelplastic.TopicMaps.SharpTM.Core to Pixelplastic.TopicMaps.SharpTM * renamed assembly title from "SharpTM Core" to "SharpTM" + added Core folder + added Index folder

--- source/SharpTM/SharpTM.Core/Properties/AssemblyInfo.cs
+++ source/SharpTM/SharpTM.Core/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Resources;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("SharpTM Core")]
+[assembly: AssemblyTitle("SharpTM")]
 [assembly: AssemblyDescription("Topic Maps Engine")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Pixelplastic")]
