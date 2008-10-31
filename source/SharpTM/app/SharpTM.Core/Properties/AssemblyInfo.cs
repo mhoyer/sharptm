d10108710078ff9e750d04f4d3b78e5a6b0fafdf
MarcelHoyer <MarcelHoyer@localhost>
2008-10-31 11:08:11 +0000

    * modified project folder structure

---
+++ source/SharpTM/app/SharpTM.Core/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Resources;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("SharpTM Core")]
+[assembly: AssemblyDescription("Topic Maps Engine")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Pixelplastic")]
+[assembly: AssemblyProduct("SharpTM")]
+[assembly: AssemblyCopyright("Copyright © Marcel Hoyer 2008")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("ddd3ab80-0cc8-4fad-9dd7-033026092acc")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
+[assembly: NeutralResourcesLanguageAttribute("en")]
