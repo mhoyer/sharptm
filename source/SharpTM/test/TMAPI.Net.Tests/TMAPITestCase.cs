742934c81d0416c4ee670ba3de6838941d339559
MarcelHoyer <MarcelHoyer@localhost>
2008-11-01 15:05:59 +0000

    + added modified TMAPITestCase.cs (source based on TMAPI.Net project)   * supports testing the SharpTM implementation * modified TMAPI.Net.Tests.csproj   + added LocatorTest.cs to project   + added TMAPITestCase.cs to project   + added TMAPI.Net project reference

---
+++ source/SharpTM/test/TMAPI.Net.Tests/TMAPITestCase.cs
@@ -0,0 +1,43 @@
+using TMAPI.Net.Core;
+
+namespace TMAPI.Net.Tests
+{
+	/// <summary>
+	/// Base super class for all tests.
+	/// </summary>
+	/// <remarks>
+	/// It will initialize a new <see cref="TopicMapSystemFactory"/> and a <see cref="ITopicMapSystem"/>.
+	/// </remarks>
+	public class TMAPITestCase
+	{
+		#region Fields
+		protected readonly ITopicMapSystem _system;
+		#endregion
+
+		#region constructor logic
+		/// <summary>
+		/// Initializes a new instance of the <see cref="TMAPITestCase"/> class.
+		/// </summary>
+		public TMAPITestCase()
+		{
+			TopicMapSystemFactory tmf = NewTopicMapSystemFactoryInstance();
+			_system = tmf.NewTopicMapSystem();
+		}
+		#endregion
+
+		#region methods
+		/// <summary>
+		/// Returns a new instance of <see cref="TopicMapSystemFactory"/>.
+		/// </summary>
+		/// <remarks>
+		/// Tries to find an implementation (subclass) of <see cref="TopicMapSystemFactory"/> 
+		/// and will invoke the <see cref="TopicMapSystemFactory.NewInstance"/> method.
+		/// </remarks>
+		/// <returns>A new instance of TopicMapSystemFactory.</returns>
+		public static TopicMapSystemFactory NewTopicMapSystemFactoryInstance()
+		{
+			return TopicMapSystemFactory.NewInstance<Pixelplastic.TopicMaps.SharpTM.Core.TopicMapSystemFactory>();
+		}
+		#endregion
+	}
+}
