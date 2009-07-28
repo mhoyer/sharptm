44edcae67728430534af15eeba6dcb0151100e96
MarcelHoyer <MarcelHoyer@localhost>
2009-07-28 22:13:29 +0000

    * modified SharpTM.Persistence.csproj to include new "from" mappers + added tests for some "from" mappers

--- source/SharpTM/test/SharpTM.Persistence.Tests/TopicMapFromDTOTests.cs
+++ source/SharpTM/test/SharpTM.Persistence.Tests/TopicMapFromDTOTests.cs
@@ -6,6 +6,8 @@
 using System;
 using Pixelplastic.TopicMaps.SharpTM.Persistence.Mapper;
 using Pixelplastic.TopicMaps.SharpTM.Persistence.Mapper.FromDTO;
+using TMAPI.Net.Core;
+using Xunit;
 using Xunit.BDDExtension;
 using Xunit.Extensions.AssertExtensions;
 
@@ -27,6 +29,55 @@ namespace Pixelplastic.TopicMaps.SharpTM.Persistence.Tests
 
 		It should_create_and_add_a_new_TMAPI_topic_map_to_the_system = 
 			() => topicMapSystem.GetTopicMap(TOPIC_MAP_SID).ShouldNotBeNull();
+	}
+
+	public class When_mapping_from_topic_map_DTO_with_topics : With_FilledTopicMapDTO
+	{
+		static ITopicMap topicMap;
+
+		Because of_mapping_the_topic_map =
+			() => topicMap = TopicMapFromDTO.Create(topicMapSystem, topicMapDTO);
+
+		It should_create_and_add_new_TMAPI_topics =
+			() => topicMap.Topics.Count.ShouldEqual(topicMapDTO.Topics.Count);
+
+		It should_map_the_SharpTM_topic =
+			() => topicMap.GetTopicBySubjectIdentifier(topicMap.CreateLocator(sharpTM.SubjectIdentifiers[0].HRef));
+	}
+
+	public class When_mapping_from_topic_map_DTO_with_associations : With_FilledTopicMapDTO
+	{
+		static ITopicMap topicMap;
+
+		Because of_mapping_the_topic_map =
+			() => topicMap = TopicMapFromDTO.Create(topicMapSystem, topicMapDTO);
+
+		It should_create_and_add_new_TMAPI_associations =
+			() => topicMap.Associations.Count.ShouldEqual(topicMapDTO.Associations.Count);
+	}
+
+	public class When_mapping_from_topic_map_DTO_with_merge_maps : With_FilledTopicMapDTO
+	{
+		Because of_mapping_the_topic_map =
+			() => TopicMapFromDTO.Create(topicMapSystem, topicMapDTO);
+
+		// TODO: It should_handle_the_merge_maps = () => 
+		[Fact(Skip = "Not implemented.")]
+		public new void Run()
+		{
+		}
+	}
+
+	public class When_mapping_from_topic_map_DTO_with_reifier : With_FilledTopicMapDTO
+	{
+		Because of_mapping_the_topic_map =
+			() => TopicMapFromDTO.Create(topicMapSystem, topicMapDTO);
+
+		// TODO: It should_handle_the_merge_maps = () => 
+		[Fact(Skip = "Not implemented.")]
+		public new void Run()
+		{
+		}
 	}
 
 }
---
+++ source/SharpTM/test/SharpTM.Persistence.Tests/TypeFromDTOTests.cs
@@ -0,0 +1,82 @@
+// <copyright file="TypeFromDTOTests.cs" company="Pixelplastic">
+// Copyright (C) Marcel Hoyer 2009. All rights reserved.
+// </copyright>
+// <author>Marcel Hoyer</author>
+// <email>mhoyer AT pixelplastic DOT de</email>
+using Pixelplastic.TopicMaps.SharpTM.Persistence.Mapper.FromDTO;
+using TMAPI.Net.Core;
+using Xunit.BDDExtension;
+using Xunit.Extensions.AssertExtensions;
+
+namespace Pixelplastic.TopicMaps.SharpTM.Persistence.Tests
+{
+	public class When_mapping_a_type_DTO : With_FilledTopicMapDTO
+	{
+		static ITopic topicType;
+		static ITopicMap topicMap;
+
+		Given a_sample_topic_map =
+			() => topicMap = topicMapSystem.CreateTopicMap("http://sharptm.de/TypeFromDTOTests");
+		Because of_mapping = () => topicType = TypeFromDTO.FindOrCreate(topicMap, projectType);
+
+		It should_create_one_and_only_one_TMAPI_topic = () => topicMap.Topics.Count.ShouldEqual(1);
+		It should_map_the_type_DTO_to_a_topic = 
+			() =>
+				{
+					topicMap.Topics[0].ItemIdentifiers.ShouldContain(LocatorFromDTO.Create(topicMap, projectType.TopicReference));
+				};
+	}
+
+	public class When_mapping_a_type_DTO_while_TMAPI_topic_already_exists_with_item_identifier : With_FilledTopicMapDTO
+	{
+		static ITopic topicType;
+		static ITopicMap topicMap;
+		static ITopic existingTopic;
+
+		Given a_sample_topic_map =
+			() => topicMap = topicMapSystem.CreateTopicMap("http://sharptm.de/TypeFromDTOTests");
+
+		Given the_existing_topic =
+			() => existingTopic = topicMap.CreateTopicByItemIdentifier(LocatorFromDTO.Create(topicMapSystem, projectType.TopicReference));
+
+		Because of_mapping = () => topicType = TypeFromDTO.FindOrCreate(topicMap, projectType);
+
+		It should_find_or_merge_the_type_DTO = () => topicType.ShouldEqual(existingTopic);
+	}
+
+
+	public class When_mapping_a_type_DTO_while_TMAPI_topic_already_exists_with_subject_identifier : With_FilledTopicMapDTO
+	{
+		static ITopic topicType;
+		static ITopicMap topicMap;
+		static ITopic existingTopic;
+
+		Given a_sample_topic_map =
+			() => topicMap = topicMapSystem.CreateTopicMap("http://sharptm.de/TypeFromDTOTests");
+
+		Given the_existing_topic =
+			() => existingTopic = topicMap.CreateTopicBySubjectIdentifier(LocatorFromDTO.Create(topicMapSystem, projectType.TopicReference));
+
+		Because of_mapping = () => topicType = TypeFromDTO.FindOrCreate(topicMap, projectType);
+
+		It should_find_or_merge_the_type_DTO = () => topicType.ShouldEqual(existingTopic);
+	}
+
+	public class When_mapping_a_type_DTO_while_TMAPI_topic_already_exists_with_subject_locator : With_FilledTopicMapDTO
+	{
+		static ITopic topicType;
+		static ITopicMap topicMap;
+		static ITopic existingTopic;
+
+		Given a_sample_topic_map =
+			() => topicMap = topicMapSystem.CreateTopicMap("http://sharptm.de/TypeFromDTOTests");
+
+		Given the_existing_topic =
+			() => existingTopic = topicMap.CreateTopicBySubjectLocator(LocatorFromDTO.Create(topicMapSystem, projectType.TopicReference));
+
+		Because of_mapping = () => topicType = TypeFromDTO.FindOrCreate(topicMap, projectType);
+
+		It should_find_or_merge_the_type_DTO = () => topicType.ShouldEqual(existingTopic);
+	}
+
+}
