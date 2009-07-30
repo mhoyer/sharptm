e46930e9703360412b559208db0ea4b764efeb0b
MarcelHoyer <MarcelHoyer@localhost>
2009-07-30 07:30:06 +0000

    * fixed Id of identifiedTopicDTO to be a valid URI

--- source/SharpTM/test/SharpTM.Persistence.Tests/With_TopicDTO.cs
+++ source/SharpTM/test/SharpTM.Persistence.Tests/With_TopicDTO.cs
@@ -26,7 +26,7 @@ namespace Pixelplastic.TopicMaps.SharpTM.Persistence.Tests
 			() =>
 				{
 					identifiedTopicDTO = new TopicDTO();
-					identifiedTopicDTO.Id = "an_identified_topic";
+					identifiedTopicDTO.Id = "http://sharptm.de/With_TopicDTO#an_identified_topic";
 					identifiedTopicDTO.SubjectIdentifiers.Add(new LocatorDTO() { HRef = "http://sharptm.de/si" });
 					identifiedTopicDTO.SubjectIdentifiers.Add(new LocatorDTO() { HRef = "http://marcelhoyer.de/si" });
 					identifiedTopicDTO.SubjectLocators.Add(new LocatorDTO() { HRef = "http://sharptm.de/sl" });
