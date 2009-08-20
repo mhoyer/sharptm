553ea63fd370204e05203b2b2a7529d66a0f0818
MarcelHoyer <MarcelHoyer@localhost>
2009-08-20 21:04:21 +0000

    + added tests for new locator validation

--- source/SharpTM/test/SharpTM.Persistence.Tests/LocatorFromDTOTests.cs
+++ source/SharpTM/test/SharpTM.Persistence.Tests/LocatorFromDTOTests.cs
@@ -3,6 +3,7 @@
 // </copyright>
 // <author>Marcel Hoyer</author>
 // <email>mhoyer AT pixelplastic DOT de</email>
+using System;
 using Pixelplastic.TopicMaps.SharpTM.Persistence.DTOs;
 using Pixelplastic.TopicMaps.SharpTM.Persistence.Mapper.FromDTO;
 using TMAPI.Net.Core;
@@ -20,4 +21,29 @@ namespace Pixelplastic.TopicMaps.SharpTM.Persistence.Tests
 		Because of_creating_an_TMAPI_instance = () => locator = LocatorFromDTO.Create(topicMapSystem, locatorDTO);
 		It should_match_the_reference_value = () => locator.Reference.ShouldEqual(TOPIC_MAP_SID);
 	}
+
+	public class When_mapping_a_relativ_locator : With_TopicMap
+	{
+		static LocatorDTO locatorDTO;
+		static ILocator locator;
+
+		Given a_relative_locator_DTO = () => locatorDTO = new LocatorDTO() { HRef = "foo" };
+		Because of_creating_an_TMAPI_locator_instance = () => locator = LocatorFromDTO.Create(topicMap, locatorDTO);
+		It should_create_the_combined_locator_with_absolute_IRI = 
+			() => locator.Reference.ShouldEqual(
+				String.Format("{0}#{1}", topicMap.ItemIdentifiers[0].Reference, locatorDTO.HRef));
+	}
+
+	public class When_mapping_a_fractalized_locator : With_TopicMap
+	{
+		static LocatorDTO locatorDTO;
+		static ILocator locator;
+
+		Given a_relative_locator_DTO = () => locatorDTO = new LocatorDTO() { HRef = "#fractal#" };
+		Because of_creating_an_TMAPI_locator_instance = () => locator = LocatorFromDTO.Create(topicMap, locatorDTO);
+		It should_create_the_combined_locator_with_absolute_IRI =
+			() => locator.Reference.ShouldEqual(
+				String.Format("{0}{1}", topicMap.ItemIdentifiers[0].Reference, locatorDTO.HRef));
+	}
+
 }
