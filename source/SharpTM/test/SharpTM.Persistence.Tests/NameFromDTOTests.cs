5a573d2cc7a508858fe1b652f769524afb410320
MarcelHoyer <MarcelHoyer@localhost>
2009-08-20 09:59:49 +0000

    + added test for NameFromDTO mapper

---
+++ source/SharpTM/test/SharpTM.Persistence.Tests/NameFromDTOTests.cs
@@ -0,0 +1,31 @@
+// <copyright file="NameFromDTOTests.cs" company="Pixelplastic">
+// Copyright (C) Marcel Hoyer 2009. All rights reserved.
+// </copyright>
+// <author>Marcel Hoyer</author>
+// <email>mhoyer AT pixelplastic DOT de</email>
+using Pixelplastic.TopicMaps.SharpTM.Persistence.Mapper.FromDTO;
+using TMAPI.Net.Core;
+using Xunit.BDDExtension;
+using Xunit.Extensions.AssertExtensions;
+
+namespace Pixelplastic.TopicMaps.SharpTM.Persistence.Tests
+{
+	public class When_mapping_a_name : With_FilledTopicMapDTOandNames
+	{
+		static ITopicMap topicMap;
+		static ITopic topic;
+		static IName name;
+		static ILocator topicName;
+
+		Given an_empty_topicMap = () => topicMap = topicMapSystem.CreateTopicMap(TestHelper.CreateLocatorString("foo")); 
+		Given an_empty_topic = () => topic = topicMap.CreateTopic();
+		Given the_TopicName_locator = () => topicName = topicMapSystem.CreateLocator("http://psi.topicmaps.org/iso13250/model/topic-name");
+
+		Because of_mapping_the_name = () => name = NameFromDTO.Create(topic, marcelHoyerName);
+
+		It should_create_the_TopicName_type = () => topicMap.GetTopicBySubjectIdentifier(topicName).ShouldNotBeNull();
+		It should_map_the_type = () => name.Type.ShouldEqual(topicMap.GetTopicBySubjectIdentifier(topicName));
+		It should_map_the_value = () => name.Value.ShouldEqual(marcelHoyerName.Value);
+		It should_map_the_variants = () => name.Variants.Count.ShouldEqual(marcelHoyerName.Variants.Count);
+	}
+}
--- source/SharpTM/test/SharpTM.Persistence.Tests/With_FilledTopicMapDTOandNames.cs
+++ source/SharpTM/test/SharpTM.Persistence.Tests/With_FilledTopicMapDTOandNames.cs
@@ -1,10 +1,18 @@
+using Pixelplastic.TopicMaps.SharpTM.Persistence.DTOs;
 using Xunit.BDDExtension;
 
 namespace Pixelplastic.TopicMaps.SharpTM.Persistence.Tests
 {
 	public abstract class With_FilledTopicMapDTOandNames : With_FilledTopicMapDTO
 	{
-		Given Names_for_Marcel_Hoyer =
-			() => marcelHoyer.Names.Add(TestHelper.CreateName("Marcel Hoyer", "Hoyer, Marcel", "Marcel"));
+		protected static NameDTO marcelHoyerName;
+
+		Given The_name_for_Marcel_Hoyer =
+			() =>
+				{
+					marcelHoyerName = TestHelper.CreateName("Marcel Hoyer");
+					marcelHoyer.Names.Add(marcelHoyerName);
+				};
+
 	}
 }
