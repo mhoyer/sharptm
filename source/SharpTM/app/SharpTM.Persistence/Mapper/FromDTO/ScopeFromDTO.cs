189214612d9cdfe7a4eb7eae4430ce35b5f2f30b
MarcelHoyer <MarcelHoyer@localhost>
2009-08-20 09:56:58 +0000

    + added Create method

--- source/SharpTM/app/SharpTM.Persistence/Mapper/FromDTO/ScopeFromDTO.cs
+++ source/SharpTM/app/SharpTM.Persistence/Mapper/FromDTO/ScopeFromDTO.cs
@@ -3,6 +3,8 @@
 // </copyright>
 // <author>Marcel Hoyer</author>
 // <email>mhoyer AT pixelplastic DOT de</email>
+using System;
+using System.Collections.Generic;
 using Pixelplastic.TopicMaps.SharpTM.Persistence.DTOs;
 using TMAPI.Net.Core;
 
@@ -31,6 +33,18 @@ namespace Pixelplastic.TopicMaps.SharpTM.Persistence.Mapper.FromDTO
 				    			scoped.AddTheme(TopicFromDTO.FindOrCreate(scoped.TopicMap, scope));
 				    		}
 				    	});
+		}
+
+		public static IList<ITopic> Create(ITopicMap parent, ScopeDTO scope)
+		{
+			IList<ITopic> scopeThemes = new List<ITopic>();
+
+			foreach (LocatorDTO dto in scope.TopicReferences)
+			{
+				scopeThemes.Add(TopicFromDTO.FindOrCreate(parent, dto));
+			}
+
+			return scopeThemes;
 		}
 	}
 }
