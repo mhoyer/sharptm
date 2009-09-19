d3ad3f7a1658cf9e231b272822a89683151f9c37
MarcelHoyer <MarcelHoyer@localhost>
2009-09-19 20:02:17 +0200

    * made auto properties to be properties with backing fields

--- source/SharpTM/app/SharpTM.Persistence.Contracts/Entities/ConstructEntity.cs
+++ source/SharpTM/app/SharpTM.Persistence.Contracts/Entities/ConstructEntity.cs
@@ -4,13 +4,32 @@
 // <author>Marcel Hoyer</author>
 // <email>mhoyer AT pixelplastic DOT de</email>
 
+using System;
 using System.Collections.Generic;
 
 namespace Pixelplastic.TopicMaps.SharpTM.Persistence.Contracts.Entities
 {
 	public class ConstructEntity
 	{
-		public virtual string Id { get; set; }
-		public virtual IList<string> ItemIdentifiers { get; set; }
+		string _id;
+		List<string> _itemIdentifiers;
+
+		public virtual string Id
+		{
+			get { return _id; }
+			set { _id = value; }
+		}
+
+		public virtual List<string> ItemIdentifiers
+		{
+			get { return _itemIdentifiers;}
+			set { _itemIdentifiers = value; }
+		}
+
+		public ConstructEntity()
+		{
+			_id = Guid.NewGuid().ToString();
+			_itemIdentifiers = new List<string>();
+		}
 	}
 }
